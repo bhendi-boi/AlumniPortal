72688c655fbd38dec0286fd0b314dea1ac51457d
bhendi-boi <bhendiisgreen@gmail.com>
2023-03-27 21:38:52 +0530

    home page newsroom and events section complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,14 @@
 import React from 'react';
+import NewsRoom from './NewsRoom';
+import Events from './Events';
 
 const page = () => {
   return (
-    <main className="grid min-h-screen place-items-center">
-      <div>
-        <h1 className="mb-8 text-5xl font-bold text-green-400">
-          Hello world 👋
-        </h1>
-        <p className="text-zinc-500">
-          If you found this template useful give it a star on{' '}
-          <a
-            target="_blank"
-            rel="norefer"
-            href="https://github.com/bhendi-boi/next13-tailwind-template"
-            className="text-xl text-blue-600 hover:text-blue-600/80"
-          >
-            github
-          </a>
-          .
-        </p>
-      </div>
+    <main className="my-20 px-10">
+      <section className="grid grid-cols-8 gap-8">
+        <NewsRoom />
+        <Events />
+      </section>
     </main>
   );
 };
