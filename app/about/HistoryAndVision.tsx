204eb0c2ba3eed161c2f8c12f3986894283b9641
bhendi-boi <bhendiisgreen@gmail.com>
2023-03-28 20:40:42 +0530

    History and Vision section in about route complete

---
+++ app/about/HistoryAndVision.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Image from 'next/image';
+const HistoryAndVision = () => {
+  return (
+    <section
+      aria-labelledby="history"
+      className="mx-auto max-w-3xl px-5 md:p-0"
+    >
+      <Image
+        src="/convocation.png"
+        alt="Convocation 2023"
+        width={711}
+        height={400}
+      />
+      <h2 className="mt-8 mb-2 text-2xl font-bold">History and Vision</h2>
+      <p>
+        The Office of Alumni Affairs was established in 20XX IIITDM works to
+        connect alumni, students and the institute to create a seamless
+        experience through various events such as Alumni Talks, Batch-wise
+        Reunions, Alumni Meets & Conferences. Our vision is to build a better
+        community among alumni and students.
+      </p>
+    </section>
+  );
+};
+
+export default HistoryAndVision;
--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import Contact from './Contact';
+import HistoryAndVision from './HistoryAndVision';
 
 const page = () => {
   return (
     <main className="min-h-screen">
+      <header className="pt-16 pb-8">
+        <h1 className="text-center text-4xl">About us</h1>
+      </header>
+      <HistoryAndVision />
       <Contact />
     </main>
   );
