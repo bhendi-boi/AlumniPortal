b1a421b407325ab929c6ba64b1c812e7846fad7f
bhendi-boi <bhendiisgreen@gmail.com>
2023-03-30 07:37:57 +0530

    Added hero section in home route

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import NewsRoom from './NewsRoom';
 import Events from './Events';
+import Image from 'next/image';
 
 const page = () => {
   return (
-    <main className="px-5 my-20 md:px-10">
-      <section className="flex flex-col gap-4 md:flex-row md:gap-8">
+    <main className="my-20 px-5 md:px-10">
+      <picture className="">
+        <Image
+          src="/HomePageHeroSection.png"
+          alt="Poster for MDes (IPD)"
+          height={424}
+          width={1360}
+        />
+      </picture>
+      <section className="mt-8 flex flex-col gap-4 md:flex-row md:gap-8">
         <NewsRoom />
         <Events />
       </section>
