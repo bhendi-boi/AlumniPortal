3461e323484fb73a4219e577eb4d96146100b357
bhendi-boi <bhendiisgreen@gmail.com>
2023-04-06 21:38:11 +0530

    removed unncesary margin in header

--- app/Header.tsx
+++ app/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = ({ title }: { title: string }) => {
   return (
-    <header className="mx-5 my-8 max-w-5xl md:mx-auto">
+    <header className="my-8 max-w-5xl md:mx-auto">
       <h1 className="text-left text-4xl">{title}</h1>
     </header>
   );
