9ea06131e12689a219b2924e690c5d6562c3cb36
bhendi-boi <bhendiisgreen@gmail.com>
2023-04-06 22:29:36 +0530

    added basic structure volunteer sub route in services route

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import type { Metadata } from 'next';
+import Header from 'app/Header';
+import NavLink from 'app/NavLink';
 
 export const metadata: Metadata = {
   title: {
@@ -9,7 +11,51 @@ export const metadata: Metadata = {
 };
 
 const page = () => {
-  return <div>Services page</div>;
+  return (
+    <main className="min-h-screen px-5 md:px-10">
+      <Header title="Alumni Services" />
+      <h2 className="my-2 text-2xl font-medium">Useful Links 👇</h2>
+      <ul className="flex max-w-5xl flex-col gap-4 md:mx-auto">
+        {/* //? Using NavLink active variant just for styling */}
+        <li>
+          <NavLink
+            variant="active"
+            title="Send Query"
+            href="/services/sendQuery"
+          >
+            Send Query
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            variant="active"
+            title="Alumni Directory"
+            href="/services/alumniDirectory"
+          >
+            Alumni Directory
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            variant="active"
+            title="Alumni Achievements"
+            href="/services/alumniAchievements"
+          >
+            Alumni Achievements
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            variant="active"
+            title="Volunteer"
+            href="/services/volunteer"
+          >
+            Volunteer
+          </NavLink>
+        </li>
+      </ul>
+    </main>
+  );
 };
 
 export default page;
---
+++ app/services/volunteer/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from 'app/Header';
+
+const page = () => {
+  return (
+    <main className="min-h-screen px-5 md:px-10">
+      <Header title="Volunteer" />
+    </main>
+  );
+};
+
+export default page;
