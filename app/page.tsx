2ac9cca63e0227a16fd8dca01ace57b407b359d3
bhendi-boi <bhendiisgreen@gmail.com>
2023-04-07 09:05:13 +0530

    changed container size in Home page

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 
 const page = () => {
   return (
-    <main className="mx-auto my-20 max-w-5xl px-5 md:px-10">
+    <main className="mx-auto my-20 max-w-7xl px-5 md:px-10">
       <picture className="">
         <Image
           src="/HomePageHeroSection.png"
