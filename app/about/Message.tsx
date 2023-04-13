365aa95d6c5e72d73368f09a7347ceea2d153863
bhendi-boi <bhendiisgreen@gmail.com>
2023-04-13 17:49:40 +0530

    code refactoring in about route - introduced a content folder specifically to store static data

--- app/about/Message.tsx
+++ app/about/Message.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { messageFromTheDirector, messageFromThePIC } from 'content/about';
 
 const Message = () => {
   return (
@@ -7,37 +8,29 @@ const Message = () => {
         aria-labelledby="message-from-director"
         className="mb-4 rounded-lg bg-secondary-background p-4 md:ml-40"
       >
+        <h2 id="message-from-director" className="sr-only">
+          Message from Director, IIITDM Kancheepuram
+        </h2>
         <p className={`before:content-['"'] after:content-['"']`}>
-          The Office of Alumni Affairs was established in 20XX IIITDM works to
-          connect alumni, students and the institute to create a seamless
-          experience through various events such as Alumni Talks, Batch-wise
-          Reunions, Alumni Meets & Conferences. Our vision is to build a better
-          community among alumni and students.
+          {messageFromTheDirector}
         </p>
         <p className="mt-2 font-bold text-contact-blue">
           Director, IIITDM Kancheepuram
         </p>
-        <h2 id="message-from-director" className="sr-only">
-          Message from Director, IIITDM Kancheepuram
-        </h2>
       </section>
       <section
         aria-labelledby="message-from-pic"
         className="rounded-lg bg-secondary-background p-4 md:mr-40"
       >
+        <h2 id="message-from-pic" className="sr-only">
+          Message from Director, IIITDM Kancheepuram
+        </h2>
         <p className={`before:content-['"'] after:content-['"']`}>
-          The Office of Alumni Affairs was established in 20XX IIITDM works to
-          connect alumni, students and the institute to create a seamless
-          experience through various events such as Alumni Talks, Batch-wise
-          Reunions, Alumni Meets & Conferences. Our vision is to build a better
-          community among alumni and students.
+          {messageFromThePIC}
         </p>
         <p className="mt-2 font-bold text-contact-blue">
           Professor In Charge (PIC)
         </p>
-        <h2 id="message-from-pic" className="sr-only">
-          Message from Director, IIITDM Kancheepuram
-        </h2>
       </section>
     </div>
   );
---
+++ content/about.ts
@@ -0,0 +1,5 @@
+export const messageFromTheDirector =
+  'The Office of Alumni Affairs was established in 20XX IIITDM works to connect alumni, students and the institute to create a seamless experience through various events such as Alumni Talks, Batch-wise reunions, Alumni Meets & Conferences. Our vision is to build a better community among alumni and students.';
+
+export const messageFromThePIC =
+  'The Office of Alumni Affairs was established in 20XX IIITDM works to connect alumni, students and the institute to create a seamless experience through various events such as Alumni Talks, Batch-wise reunions, Alumni Meets & Conferences. Our vision is to build a better community among alumni and students.';
