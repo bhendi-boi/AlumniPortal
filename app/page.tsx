20b6d42efd1a52e9e07f93f41eb6a4845874096a
bhendi-boi <bhendiisgreen@gmail.com>
2023-06-09 17:17:31 +0530

    renamed getData to getActivities

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,14 @@ import Activities from './Activities';
 import Carousel from './Carousel';
 import { supabase } from './supabase';
 
-async function getData() {
+async function getActivities() {
   const { data, error } = await supabase.from('activities').select();
 
   return data;
 }
 
 const page = async () => {
-  const data = await getData();
+  const data = await getActivities();
   return (
     <div className="my-20">
       {/* <Carousel /> */}
