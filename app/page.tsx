7f24a16d9706b10e372a74ea1c1214b542f427e9
bhendi-boi <bhendiisgreen@gmail.com>
2023-06-11 22:35:51 +0530

    added a comment in page / route

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ const page = async () => {
   return (
     <div className="my-20">
       {/* <Carousel /> */}
+      {/* these comments are there so that we can use rsc. Remove them after ts fixes this */}
       <section className="mt-8 flex flex-col gap-4 md:flex-row md:gap-8">
         {/* @ts-expect-error */}
         <NewsRoom />
