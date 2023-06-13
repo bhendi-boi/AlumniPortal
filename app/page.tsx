d95cb295915aef5da18d8d937dc1f28dc81e80f7
bhendi-boi <bhendiisgreen@gmail.com>
2023-06-13 15:15:31 +0530

    updated react types. Can use rsc now

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,9 @@ import Carousel from './Carousel';
 const page = async () => {
   return (
     <div className="my-20">
-      {/* @ts-expect-error */}
       <Carousel />
-      {/* these comments are there so that we can use rsc. Remove them after ts fixes this */}
       <section className="mt-8 flex flex-col gap-4 md:flex-row md:gap-8">
-        {/* @ts-expect-error */}
         <NewsRoom />
-        {/* @ts-expect-error */}
         <Activities />
       </section>
     </div>
