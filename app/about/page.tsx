9f253ded12c7b7867ff0c5ac02af20af39f14251
bhendi-boi <bhendiisgreen@gmail.com>
2023-06-14 23:30:57 +0530

    updated og metadata for /about route

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,14 +7,28 @@ import Team from './Team';
 export const metadata: Metadata = {
   title: 'About',
   description:
-    "This webpage contains a detailed overview of the Alumni Affairs, IIITDM's mission, goals, and values, as well as a brief history of its founding and evolution over time. Visitors to the webpage can learn about the current leadership team, including their roles and responsibilities, as well as the members who make up the organization.",
+    "This webpage contains a detailed overview of the Alumni Affairs, IIITDM's mission, goals and values, as well as a brief history of its founding and evolution over time. Visitors to the webpage can learn about the current leadership team, including their roles and responsibilities, as well as the members who make up the organization.",
+  openGraph: {
+    title: 'About | Alumni Portal',
+    description:
+      "This webpage contains a detailed overview of the Alumni Affairs, IIITDM's mission, goals and values, as well as a brief history of its founding and evolution over time. Visitors to the webpage can learn about the current leadership team, including their roles and responsibilities, as well as the members who make up the organization",
+    url: 'https://alumni-portal-alpha.vercel.app/about',
+    siteName: 'Alumni Portal',
+    images: [
+      {
+        url: '/og-image.png',
+      },
+    ],
+    locale: 'en-IN',
+    type: 'website',
+  },
 };
 
 const page = () => {
   return (
     <>
-      <header className="pb-8 pt-16">
-        <h1 className="text-center text-4xl">About us</h1>
+      <header className="pt-16 pb-8">
+        <h1 className="text-4xl text-center">About us</h1>
       </header>
       <HistoryAndVision />
       <Message />
