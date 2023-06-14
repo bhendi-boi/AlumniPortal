6b56ad67c7d7a11367c17f6fbba801d7b2ab2c46
bhendi-boi <bhendiisgreen@gmail.com>
2023-06-14 23:41:09 +0530

    updated og metadata for /alumniDirectory route

--- app/alumniDirectory/page.tsx
+++ app/alumniDirectory/page.tsx
@@ -3,10 +3,23 @@ import Header from 'app/Header';
 import Directory from './Directory';
 
 export const metadata: Metadata = {
-  title: {
-    absolute: 'Alumni Directory | IIITDM',
+  title: 'Directory',
+  description:
+    'Find alumni from your school with our alumni directory. Search by name, graduation year, or major to find former classmates and other alumni. Our directory is the perfect way to stay connected with iiitdm community and reconnect with old friends.',
+  openGraph: {
+    title: 'Directory | Alumni Portal',
+    description:
+      'Find alumni from your school with our alumni directory. Search by name, graduation year, or major to find former classmates and other alumni. Our directory is the perfect way to stay connected with iiitdm community and reconnect with old friends.',
+    url: 'https://alumni-portal-alpha.vercel.app/alumniDirectory',
+    siteName: 'Alumni Portal',
+    images: [
+      {
+        url: '/og-image.png',
+      },
+    ],
+    locale: 'en-IN',
+    type: 'website',
   },
-  description: 'Directory of our alumni.',
 };
 
 const page = () => {
