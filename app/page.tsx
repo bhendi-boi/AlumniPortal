d9a8fb2d4269d9ee0a3b92af0f5bc7fe3eb2d4a0
bhendi-boi <bhendiisgreen@gmail.com>
2023-06-14 23:20:35 +0530

    changed og title for / route

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,17 @@
 import NewsRoom from './NewsRoom';
 import Activities from './Activities';
 import Carousel from './Carousel';
+export const metadata = {
+  openGraph: {
+    title: 'Home | Alumni Portal',
+  },
+};
 
 const page = async () => {
   return (
     <div className="my-20">
       <Carousel />
-      <section className="mt-8 flex flex-col gap-4 md:flex-row md:gap-8">
+      <section className="flex flex-col gap-4 mt-8 md:flex-row md:gap-8">
         <NewsRoom />
         <Activities />
       </section>
