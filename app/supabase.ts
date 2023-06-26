b3ecccd1430abdd5f54654def2f893a0f782733c
bhendi-boi <bhendiisgreen@gmail.com>
2023-06-26 20:15:11 +0530

    added supabaseKey to env

--- app/supabase.ts
+++ app/supabase.ts
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js';
 import { Database } from 'types';
 
 const supabaseUrl = 'https://ocufbllgoonzjowlkkhp.supabase.co';
-const supabaseKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9jdWZibGxnb29uempvd2xra2hwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODYyNDAzOTYsImV4cCI6MjAwMTgxNjM5Nn0.7AvANumEf_Xa7BuuRGoYjjgDAQmkHbIooENloU1AwDc';
+const supabaseKey = process.env.SUPABASE_KEY as string;
+
 export const supabase = createClient<Database>(supabaseUrl, supabaseKey);
