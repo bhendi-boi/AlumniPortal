90949a356fa1eebadce18a43bf2543f2c17011bc
bhendi-boi <bhendiisgreen@gmail.com>
2023-06-29 17:52:48 +0530

    added ordered by methods in fetch calls

--- app/events/fetchers.ts
+++ app/events/fetchers.ts
@@ -1,7 +1,10 @@
 import { supabase } from 'app/supabase';
 
 export async function getActivitiesData() {
-  const { data, error } = await supabase.from('activities').select();
+  const { data, error } = await supabase
+    .from('activities')
+    .select()
+    .order('time', { ascending: false });
   return { data, error };
 }
 export async function getAnActivityData(id: number) {
--- app/services/newsroom/fetchers.ts
+++ app/services/newsroom/fetchers.ts
@@ -1,7 +1,10 @@
 import { supabase } from 'app/supabase';
 
 export async function getNewsArticleData() {
-  const { data, error } = await supabase.from('newsroom').select();
+  const { data, error } = await supabase
+    .from('newsroom')
+    .select()
+    .order('created_at', { ascending: false });
   return { data, error };
 }
 
