3ca8e6e5f85d5a3d71d94b7062946fb6e6a53764
bhendi-boi <bhendiisgreen@gmail.com>
2023-07-08 14:47:12 +0530

    added YTPlayer component

---
+++ app/YTPlayer.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const YTPlayer = ({ url }: { url: string }) => {
+  const sanitizedUrl = 'https://www.youtube.com/embed/' + url.split('/').pop();
+  return (
+    <div className="mx-auto my-8 max-w-xl overflow-hidden rounded-lg bg-gradient-to-br from-gray-100/80 via-gray-200/50 to-gray-400/10">
+      <iframe
+        title="video"
+        src={sanitizedUrl}
+        allow="autoplay; picture-in-picture"
+        className="aspect-video w-full"
+      ></iframe>
+    </div>
+  );
+};
+
+export default YTPlayer;
