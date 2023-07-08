082ccd5f28b22f3ced570f7dfedb1bf9eec5a777
bhendi-boi <bhendiisgreen@gmail.com>
2023-07-08 15:44:09 +0530

    changed supabase client to the one first used

--- app/supabase.ts
+++ app/supabase.ts
@@ -1,5 +1,7 @@
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
-import { cookies } from 'next/headers';
+import { createClient } from '@supabase/supabase-js';
 import { Database } from 'types';
 
-export const supabase = createServerComponentClient<Database>({ cookies });
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+
+export const supabase = createClient<Database>(supabaseUrl, supabaseKey);
