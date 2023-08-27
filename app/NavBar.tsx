522eed35869d66e8fb97234abde12562890ee757
bhendi-boi <bhendiisgreen@gmail.com>
2023-08-27 08:08:42 +0530

    Navbar is identical to IITM's alumni website

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -14,12 +14,13 @@ import clsx from 'clsx';
 const NavBar = () => {
   const path = useSelectedLayoutSegment();
   const [isOpen, setIsOpen] = useState(false);
+  // ? showShadow is also used to toggle b/w showing logo + navbar and showing only navbar
   const [showShadow, setShowShadow] = useState(false);
 
   useEffect(() => {
     if (!window) return;
     const handler = () => {
-      if (window.scrollY !== 0) {
+      if (window.scrollY >= 300) {
         setShowShadow(true);
       } else {
         setShowShadow(false);
@@ -36,206 +37,300 @@ const NavBar = () => {
     else body.classList.remove('overflow-y-hidden');
   }, [isOpen]);
   return (
-    <header
-      className={clsx(
-        'z-10 flex h-16 items-center border-b border-background bg-white px-5 md:h-52 md:flex-col md:items-stretch md:px-10 md:pt-5',
-        showShadow && 'shadow-lg',
-      )}
-    >
-      <div className="flex flex-1 items-center gap-4 md:gap-0">
-        <Link href="https://iiitdm.ac.in" rel="norefer" target="_blank">
-          <Image
-            className="w-10 md:w-24"
-            src={'/iiitdm.png'}
-            alt="IIITDM Logo"
-            width={90}
-            height={90}
-          />
-        </Link>
-        <Link href="/">
-          <Image
-            priority
-            className="block md:hidden"
-            src={'/aa_logo.png'}
-            alt="Alumni Affairs IIITDM Logo"
-            width={40}
-            height={40}
-          />
-        </Link>
-        <Link href="/">
-          <Image
-            priority
-            className="hidden md:block"
-            src={'/logo_longclg2.png'}
-            alt="Alumni Affairs IIITDM Logo"
-            width={400}
-            height={400}
-          />
-        </Link>
-      </div>
-      <nav className="hidden flex-1 flex-wrap items-center justify-between gap-4 px-4 md:flex">
-        <NavLink
-          href="/"
-          variant={path === null ? 'active' : 'primary'}
-          title="Home"
-        >
-          Home
-        </NavLink>
-        <NavLink
-          href="/about"
-          variant={path === 'about' ? 'active' : 'primary'}
-          title="About"
-        >
-          About
-        </NavLink>
-        <NavLink
-          href="/events"
-          variant={path === 'events' ? 'active' : 'primary'}
-          title="Events"
-        >
-          Events
-        </NavLink>
-        <NavLink
-          href="/gallery"
-          variant={path === 'gallery' ? 'active' : 'primary'}
-          title="Gallery"
-        >
-          Gallery
-        </NavLink>
-        <NavLink
-          href="/alumniDirectory"
-          variant={path === 'alumniDirectory' ? 'active' : 'primary'}
-          title="Alumni Directory"
-        >
-          Directory
-        </NavLink>
-        <NavLink
-          href="/newsroom"
-          variant={path === 'newsroom' ? 'active' : 'primary'}
-          title="Newsroom"
-        >
-          Newsroom
-        </NavLink>
-        <NavLink
-          href="/contact"
-          variant={path === 'contact' ? 'active' : 'primary'}
-          title="Newsroom"
-        >
-          Contact Us
-        </NavLink>
-        <NavLink
-          href="/alumniFund"
-          variant={path === 'alumniFund' ? 'active' : 'primary'}
-          title="Alumni Fund"
-        >
-          Alumni Fund
-        </NavLink>
-      </nav>
-      {/* mobile nav */}
-      <Menu>
-        <Menu.Button
-          className="md:hidden"
-          title="Menu button"
-          onClick={() => setIsOpen((prev) => !prev)}
-        >
-          {isOpen ? <GrFormClose size={30} /> : <HiMenuAlt4 size={30} />}
-        </Menu.Button>
-        <AnimatePresence>
-          {isOpen && (
-            <Menu.Items
-              static
-              onClick={() => setIsOpen(false)}
-              className="absolute right-0 top-16 z-10 h-screen w-full overflow-hidden bg-neutral-950/30"
+    <>
+      <header
+        className={clsx(
+          'sticky top-0 z-10 flex h-16 items-center justify-center border-b bg-white px-5 md:static  md:h-auto  md:flex-col md:items-stretch md:px-10 md:pt-5',
+          showShadow && 'shadow-lg md:shadow-none',
+        )}
+      >
+        <div className="flex flex-1 items-center gap-4 md:gap-0">
+          <Link href="https://iiitdm.ac.in" rel="norefer" target="_blank">
+            <Image
+              className="w-10 md:w-24"
+              src={'/iiitdm.png'}
+              alt="IIITDM Logo"
+              width={90}
+              height={90}
+            />
+          </Link>
+          <Link href="/">
+            <Image
+              priority
+              className="block md:hidden"
+              src={'/aa_logo.png'}
+              alt="Alumni Affairs IIITDM Logo"
+              width={40}
+              height={40}
+            />
+          </Link>
+          <Link href="/">
+            <Image
+              priority
+              className="hidden md:block"
+              src={'/logo_longclg2.png'}
+              alt="Alumni Affairs IIITDM Logo"
+              width={400}
+              height={400}
+            />
+          </Link>
+        </div>
+
+        {!showShadow && (
+          <motion.nav
+            key="navbar"
+            initial={{ y: '0', opacity: 1 }}
+            exit={{
+              y: '-10vh',
+              opacity: 0,
+            }}
+            className={clsx(
+              'hidden flex-1 flex-wrap items-center justify-between gap-4 px-4 py-4',
+              !showShadow && 'md:flex',
+            )}
+          >
+            <NavLink
+              href="/"
+              variant={path === null ? 'active' : 'primary'}
+              title="Home"
             >
-              <motion.ul
-                initial={{ x: '100%', opacity: 0 }}
-                animate={{ x: 0, opacity: 1 }}
-                exit={{ x: '100%', opacity: 1 }}
-                transition={{ type: 'tween', duration: 0.25 }}
-                className="absolute right-0 top-0 flex h-screen w-2/3 flex-col gap-4 bg-background p-6"
+              Home
+            </NavLink>
+            <NavLink
+              href="/about"
+              variant={path === 'about' ? 'active' : 'primary'}
+              title="About"
+            >
+              About
+            </NavLink>
+            <NavLink
+              href="/events"
+              variant={path === 'events' ? 'active' : 'primary'}
+              title="Events"
+            >
+              Events
+            </NavLink>
+            <NavLink
+              href="/gallery"
+              variant={path === 'gallery' ? 'active' : 'primary'}
+              title="Gallery"
+            >
+              Gallery
+            </NavLink>
+            <NavLink
+              href="/alumniDirectory"
+              variant={path === 'alumniDirectory' ? 'active' : 'primary'}
+              title="Alumni Directory"
+            >
+              Directory
+            </NavLink>
+            <NavLink
+              href="/newsroom"
+              variant={path === 'newsroom' ? 'active' : 'primary'}
+              title="Newsroom"
+            >
+              Newsroom
+            </NavLink>
+            <NavLink
+              href="/contact"
+              variant={path === 'contact' ? 'active' : 'primary'}
+              title="Newsroom"
+            >
+              Contact Us
+            </NavLink>
+            <NavLink
+              href="/alumniFund"
+              variant={path === 'alumniFund' ? 'active' : 'primary'}
+              title="Alumni Fund"
+            >
+              Alumni Fund
+            </NavLink>
+          </motion.nav>
+        )}
+
+        {/* mobile nav */}
+        <Menu>
+          <Menu.Button
+            className="md:hidden"
+            title="Menu button"
+            onClick={() => setIsOpen((prev) => !prev)}
+          >
+            {isOpen ? <GrFormClose size={30} /> : <HiMenuAlt4 size={30} />}
+          </Menu.Button>
+          <AnimatePresence>
+            {isOpen && (
+              <Menu.Items
+                static
+                onClick={() => setIsOpen(false)}
+                className="absolute right-0 top-16 z-10 h-screen w-full overflow-hidden bg-neutral-950/30"
               >
-                <Menu.Item as="li" className="">
-                  <NavLink
-                    onClick={() => setIsOpen(false)}
-                    href="/"
-                    variant={path === null ? 'active' : 'primary'}
-                    title="Home"
-                  >
-                    Home
-                  </NavLink>
-                </Menu.Item>
-                <Menu.Item as="li">
-                  <NavLink
-                    onClick={() => setIsOpen(false)}
-                    href="/about"
-                    variant={path === 'about' ? 'active' : 'primary'}
-                    title="About"
-                  >
-                    About
-                  </NavLink>
-                </Menu.Item>
-                <Menu.Item as="li">
-                  <NavLink
-                    onClick={() => setIsOpen(false)}
-                    href="/events"
-                    variant={path === 'events' ? 'active' : 'primary'}
-                    title="Events"
-                  >
-                    Events
-                  </NavLink>
-                </Menu.Item>
-                <Menu.Item as="li">
-                  <NavLink
-                    onClick={() => setIsOpen(false)}
-                    href="/gallery"
-                    variant={path === 'gallery' ? 'active' : 'primary'}
-                    title="Gallery"
-                  >
-                    Gallery
-                  </NavLink>
-                </Menu.Item>
-                <Menu.Item as="li">
-                  <NavLink
-                    href="/alumniDirectory"
-                    variant={path === 'alumniDirectory' ? 'active' : 'primary'}
-                    title="Alumni Directory"
-                  >
-                    Directory
-                  </NavLink>
-                </Menu.Item>
-                <Menu.Item as="li">
-                  <NavLink
-                    onClick={() => setIsOpen(false)}
-                    href="/newsroom"
-                    title="Newsroom"
-                  >
-                    Newsroom
-                  </NavLink>
-                </Menu.Item>
-                <Menu.Item as="li">
-                  <NavLink
-                    onClick={() => setIsOpen(false)}
-                    href="/contact"
-                    title="Contact"
-                  >
-                    Contact
-                  </NavLink>
-                </Menu.Item>
-                <Menu.Item as="li">
-                  <NavLink
-                    onClick={() => setIsOpen(false)}
-                    href="/alumniFund"
-                    title="Alumni Fund"
-                  >
-                    Alumni Fund
-                  </NavLink>
-                </Menu.Item>
-              </motion.ul>
-            </Menu.Items>
+                <motion.ul
+                  initial={{ x: '100%', opacity: 0 }}
+                  animate={{ x: 0, opacity: 1 }}
+                  exit={{ x: '100%', opacity: 1 }}
+                  transition={{ type: 'tween', duration: 0.25 }}
+                  className="absolute right-0 top-0 flex h-screen w-2/3 flex-col gap-4 bg-background p-6"
+                >
+                  <Menu.Item as="li" className="">
+                    <NavLink
+                      onClick={() => setIsOpen(false)}
+                      href="/"
+                      variant={path === null ? 'active' : 'primary'}
+                      title="Home"
+                    >
+                      Home
+                    </NavLink>
+                  </Menu.Item>
+                  <Menu.Item as="li">
+                    <NavLink
+                      onClick={() => setIsOpen(false)}
+                      href="/about"
+                      variant={path === 'about' ? 'active' : 'primary'}
+                      title="About"
+                    >
+                      About
+                    </NavLink>
+                  </Menu.Item>
+                  <Menu.Item as="li">
+                    <NavLink
+                      onClick={() => setIsOpen(false)}
+                      href="/events"
+                      variant={path === 'events' ? 'active' : 'primary'}
+                      title="Events"
+                    >
+                      Events
+                    </NavLink>
+                  </Menu.Item>
+                  <Menu.Item as="li">
+                    <NavLink
+                      onClick={() => setIsOpen(false)}
+                      href="/gallery"
+                      variant={path === 'gallery' ? 'active' : 'primary'}
+                      title="Gallery"
+                    >
+                      Gallery
+                    </NavLink>
+                  </Menu.Item>
+                  <Menu.Item as="li">
+                    <NavLink
+                      href="/alumniDirectory"
+                      variant={
+                        path === 'alumniDirectory' ? 'active' : 'primary'
+                      }
+                      title="Alumni Directory"
+                    >
+                      Directory
+                    </NavLink>
+                  </Menu.Item>
+                  <Menu.Item as="li">
+                    <NavLink
+                      onClick={() => setIsOpen(false)}
+                      href="/newsroom"
+                      title="Newsroom"
+                    >
+                      Newsroom
+                    </NavLink>
+                  </Menu.Item>
+                  <Menu.Item as="li">
+                    <NavLink
+                      onClick={() => setIsOpen(false)}
+                      href="/contact"
+                      title="Contact"
+                    >
+                      Contact
+                    </NavLink>
+                  </Menu.Item>
+                  <Menu.Item as="li">
+                    <NavLink
+                      onClick={() => setIsOpen(false)}
+                      href="/alumniFund"
+                      title="Alumni Fund"
+                    >
+                      Alumni Fund
+                    </NavLink>
+                  </Menu.Item>
+                </motion.ul>
+              </Menu.Items>
+            )}
+          </AnimatePresence>
+        </Menu>
+      </header>
+
+      {showShadow && (
+        <motion.nav
+          initial={{ y: '-10vh', opacity: 0 }}
+          animate={{
+            y: 0,
+            opacity: 1,
+          }}
+          transition={{ duration: 0.3, type: 'tween', ease: 'easeInOut' }}
+          key="navbar"
+          className={clsx(
+            'sticky top-0 z-10 hidden flex-wrap items-center justify-between gap-4 bg-white px-10 py-4',
+
+            showShadow && 'shadow-lg md:flex',
           )}
-        </AnimatePresence>
-      </Menu>
-    </header>
+        >
+          {' '}
+          <NavLink
+            href="/"
+            variant={path === null ? 'active' : 'primary'}
+            title="Home"
+          >
+            Home
+          </NavLink>
+          <NavLink
+            href="/about"
+            variant={path === 'about' ? 'active' : 'primary'}
+            title="About"
+          >
+            About
+          </NavLink>
+          <NavLink
+            href="/events"
+            variant={path === 'events' ? 'active' : 'primary'}
+            title="Events"
+          >
+            Events
+          </NavLink>
+          <NavLink
+            href="/gallery"
+            variant={path === 'gallery' ? 'active' : 'primary'}
+            title="Gallery"
+          >
+            Gallery
+          </NavLink>
+          <NavLink
+            href="/alumniDirectory"
+            variant={path === 'alumniDirectory' ? 'active' : 'primary'}
+            title="Alumni Directory"
+          >
+            Directory
+          </NavLink>
+          <NavLink
+            href="/newsroom"
+            variant={path === 'newsroom' ? 'active' : 'primary'}
+            title="Newsroom"
+          >
+            Newsroom
+          </NavLink>
+          <NavLink
+            href="/contact"
+            variant={path === 'contact' ? 'active' : 'primary'}
+            title="Newsroom"
+          >
+            Contact Us
+          </NavLink>
+          <NavLink
+            href="/alumniFund"
+            variant={path === 'alumniFund' ? 'active' : 'primary'}
+            title="Alumni Fund"
+          >
+            Alumni Fund
+          </NavLink>
+        </motion.nav>
+      )}
+    </>
   );
 };
 
