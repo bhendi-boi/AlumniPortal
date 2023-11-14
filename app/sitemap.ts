5386caf5a0760ed4437fc4ad3ec7511cefd7b265
bhendi-boi <bhendiisgreen@gmail.com>
2023-11-14 21:46:00 +0530

    added articles and events to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,5 +1,8 @@
 import { MetadataRoute } from 'next';
-export default function sitemap(): MetadataRoute.Sitemap {
+import { getActivitiesData } from './events/fetchers';
+import { getNewsArticleData } from './newsroom/fetchers';
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const sitemap: MetadataRoute.Sitemap = [
     {
       url: 'https://alumniportal.vercel.app/',
@@ -50,6 +53,38 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.8,
     },
   ];
+
+  // adding activities / events to the sitemap
+  const { data: activities, error: error1 } = await getActivitiesData();
+  if (error1 || !activities) {
+    return sitemap;
+  }
+  activities.map((activity) => {
+    const { id } = activity;
+    const sitemapEntry = {
+      url: `https://alumniportal.vercel.app/events/${id}`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly' as const,
+      priority: 0.5,
+    };
+    sitemap.push(sitemapEntry);
+  });
+
+  // adding activities / events to the sitemap
+  const { data: articles, error: error2 } = await getNewsArticleData();
+  if (error2 || !articles) {
+    return sitemap;
+  }
+  activities.map((activity) => {
+    const { id } = activity;
+    const sitemapEntry = {
+      url: `https://alumniportal.vercel.app/newsroom/${id}`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly' as const,
+      priority: 0.5,
+    };
+    sitemap.push(sitemapEntry);
+  });
 
   return sitemap;
 }
