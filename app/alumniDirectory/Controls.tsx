171791c635efb3eed8edfd2576e1fee1aa77f046
bhendi-boi <bhendiisgreen@gmail.com>
2023-12-16 15:17:50 +0530

    filter menu is responsive now

--- app/alumniDirectory/Controls.tsx
+++ app/alumniDirectory/Controls.tsx
@@ -1,13 +1,15 @@
 'use client';
 import { Dialog } from '@headlessui/react';
 import { ComponentProps, useState } from 'react';
-import { AiOutlineLeft, AiOutlineRight } from 'react-icons/ai';
-import { BsFilter } from 'react-icons/bs';
-
-import { FIRSTPASSOUTYEAR, LATESTPASSOUTYEAR } from './Directory';
-import clsx from 'clsx';
 import { usePathname, useRouter } from 'next/navigation';
+import clsx from 'clsx';
+
+import { BsFilter } from 'react-icons/bs';
+import { GrFormClose } from 'react-icons/gr';
+import { AiOutlineLeft, AiOutlineRight } from 'react-icons/ai';
+
 import { PossibleDegrees } from 'types';
+import { FIRSTPASSOUTYEAR, LATESTPASSOUTYEAR } from './Directory';
 
 const Controls = ({
   year,
@@ -27,17 +29,24 @@ const Controls = ({
     'B. Tech.',
     'M. Tech.',
     'Dual Degree',
-    'M. Des.',
     'PhD',
+    'M. Des.',
   ];
 
   const router = useRouter();
   const pathname = usePathname();
 
   return (
-    <header className="flex items-center border-b border-background px-5 py-4 text-secondary-text md:px-10">
+    <header className="flex flex-col gap-2 border-b border-background px-5 py-4 text-secondary-text sm:flex-row sm:items-center md:px-10">
       <div className="relative flex flex-1 items-center gap-4">
-        <h2 className="flex-1 text-lg font-extrabold">{year}</h2>
+        <h2 className="text-lg font-extrabold">{year}</h2>
+        <Button
+          onClick={() => {
+            setIsMenuOpen((prev) => !prev);
+          }}
+        >
+          <BsFilter size={24} />
+        </Button>
         <div>
           {degree && (
             <span className="rounded-md bg-blue-600 px-2 py-1 text-white">
@@ -45,79 +54,87 @@ const Controls = ({
             </span>
           )}
         </div>
-        <button
-          onClick={() => {
-            setIsMenuOpen((prev) => !prev);
-          }}
+
+        <Dialog
+          open={isMenuOpen}
+          onClose={() => setIsMenuOpen(false)}
+          className="relative z-50"
         >
-          <BsFilter size={24} />
-        </button>
-        <p className="text-center">Showing 1 to 25 out of 80</p>
-        <Dialog open={isMenuOpen} onClose={() => setIsMenuOpen(false)}>
-          <Dialog.Panel className="absolute right-[20%] top-3/4 w-96 rounded-md bg-white p-4 ring-2 ring-black/5">
-            <Dialog.Title className="mb-2 text-lg font-semibold">
-              Choose Filters
-            </Dialog.Title>
-            <div className="flex flex-wrap gap-1">
-              {possibleYears.map((possibleYear) => {
-                return (
-                  <button
-                    key={possibleYear}
-                    onClick={() =>
-                      router.push(
-                        pathname +
-                          '?' +
-                          createQueryString('year', possibleYear.toString()),
-                      )
-                    }
-                    className={clsx(
-                      'rounded-md  px-2 py-1 transition-colors duration-150',
-                      possibleYear === year && 'bg-blue-600 text-white',
-                      possibleYear !== year &&
-                        'bg-secondary-background text-secondary-text',
-                    )}
-                  >
-                    {possibleYear}
-                  </button>
-                );
-              })}
-            </div>
-            <div className="mt-4 flex flex-wrap gap-1">
-              {possibleDegrees.map((possibleDegree) => {
-                return (
-                  <button
-                    key={possibleDegree}
-                    onClick={() =>
-                      router.push(
-                        pathname +
-                          '?' +
-                          createQueryString('degree', possibleDegree),
-                      )
-                    }
-                    className={clsx(
-                      'rounded-md  px-2 py-1 transition-colors duration-150',
-                      possibleDegree === degree && 'bg-blue-600 text-white',
-                      possibleDegree !== degree &&
-                        'bg-secondary-background text-secondary-text',
-                    )}
-                  >
-                    {possibleDegree}
-                  </button>
-                );
-              })}
-            </div>
-          </Dialog.Panel>
+          <div aria-hidden className="fixed inset-0 bg-black/30 blur-3xl"></div>
+          <div className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+            <Dialog.Panel className="z-20 w-96 max-w-[85vw] rounded-md bg-white p-4 ring-2 ring-black/5">
+              <div className="mb-4 flex items-center">
+                <Dialog.Title className="flex-1 text-lg font-semibold ">
+                  Choose Filters
+                </Dialog.Title>
+                <Button onClick={() => setIsMenuOpen(false)}>
+                  <GrFormClose size={24} />
+                </Button>
+              </div>
+              <div className="flex flex-wrap gap-1">
+                {possibleYears.map((possibleYear) => {
+                  return (
+                    <button
+                      key={possibleYear}
+                      onClick={() =>
+                        router.push(
+                          pathname +
+                            '?' +
+                            createQueryString('year', possibleYear.toString()),
+                        )
+                      }
+                      className={clsx(
+                        'rounded-md  px-2 py-1 transition-colors duration-150',
+                        possibleYear === year && 'bg-blue-600 text-white',
+                        possibleYear !== year &&
+                          'bg-secondary-background text-secondary-text',
+                      )}
+                    >
+                      {possibleYear}
+                    </button>
+                  );
+                })}
+              </div>
+              <div className="mt-4 flex flex-wrap gap-1">
+                {possibleDegrees.map((possibleDegree) => {
+                  return (
+                    <button
+                      key={possibleDegree}
+                      onClick={() =>
+                        router.push(
+                          pathname +
+                            '?' +
+                            createQueryString('degree', possibleDegree),
+                        )
+                      }
+                      className={clsx(
+                        'rounded-md  px-2 py-1 transition-colors duration-150',
+                        possibleDegree === degree && 'bg-blue-600 text-white',
+                        possibleDegree !== degree &&
+                          'bg-secondary-background text-secondary-text',
+                      )}
+                    >
+                      {possibleDegree}
+                    </button>
+                  );
+                })}
+              </div>
+            </Dialog.Panel>
+          </div>
         </Dialog>
       </div>
 
       {/* will use this buttons to move to the next set in same year rather than next year */}
-      <div className="">
-        <Button onClick={() => {}}>
-          <AiOutlineLeft size={24} />
-        </Button>
-        <Button onClick={() => {}}>
-          <AiOutlineRight size={24} />
-        </Button>
+      <div className="flex items-center gap-2">
+        <p className="flex-1 text-sm">Showing 1 to 10 of 21</p>
+        <div>
+          <Button onClick={() => {}}>
+            <AiOutlineLeft size={24} />
+          </Button>
+          <Button onClick={() => {}}>
+            <AiOutlineRight size={24} />
+          </Button>
+        </div>
       </div>
     </header>
   );
