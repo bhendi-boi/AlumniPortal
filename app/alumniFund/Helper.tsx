a8d7a88d03f0205619219dbf0391f06e8e2a671b
bhendi-boi <bhendiisgreen@gmail.com>
2024-01-03 21:19:05 +0530

    - Image in `alumniFund` route is responsive now  - Added extra button to copy UPI id

---
+++ app/alumniFund/Helper.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { MdContentCopy } from 'react-icons/md';
+import { copyToClipboard } from './copyToClipboard';
+
+const Helper = () => {
+  return (
+    <div className="flex items-center gap-4 pb-4 text-xl">
+      <h2 className="font-medium">UPI Id:</h2>
+      <p>projectaccounts@sbi</p>
+      <Button
+        onClick={(e: React.MouseEvent) => {
+          copyToClipboard('projectaccounts@sbi');
+        }}
+      >
+        <MdContentCopy size={24} />
+      </Button>
+    </div>
+  );
+};
+
+export default Helper;
+
+function Button({ children, ...props }: React.ComponentProps<'button'>) {
+  return (
+    <button
+      {...props}
+      className="rounded-full p-2 transition-colors hover:bg-zinc-200 hover:text-black active:bg-zinc-100 active:text-black"
+    >
+      {children}
+    </button>
+  );
+}
--- app/alumniFund/page.tsx
+++ app/alumniFund/page.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import Header from 'app/Header';
-import FAQ from './FAQ';
+import Helper from './Helper';
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -24,13 +24,14 @@ const page = () => {
   return (
     <>
       <Header title="Alumni Fund" />
-      <section className="mx-auto max-w-7xl py-10">
+      <section className="mx-auto max-w-7xl pb-16">
+        <Helper />
         <Image
           src="/alumniFundUPIid.jpg"
           alt="An image showing Alumni Fund UPI Id and QR code"
           width={1833}
           height={2553}
-          className="max-w-xl rounded-lg"
+          className="max-w-full rounded-lg md:max-w-xl"
         />
       </section>
       {/* <FAQ /> */}
