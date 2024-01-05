5e606dc20a1f6680611753ac1d740f99ff67a10d
bhendi-boi <bhendiisgreen@gmail.com>
2024-01-05 15:52:22 +0530

    removed `Helper` and UPIid image from `alumniFund` route

--- app/alumniFund/page.tsx
+++ app/alumniFund/page.tsx
@@ -1,6 +1,4 @@
-import Image from 'next/image';
 import Header from 'app/Header';
-import Helper from './Helper';
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -25,24 +23,15 @@ const page = () => {
     <>
       <Header title="Alumni Fund" />
       <section className="mx-auto max-w-7xl pb-16">
-        <Helper />
-        <div className="grid gap-4 sm:gap-6 md:grid-cols-2 md:gap-8 lg:gap-12">
-          <iframe
-            src="https://docs.google.com/forms/d/e/1FAIpQLSdan_VViGmDcIufVM2EG-1E_i-Vr37jgMQMwG34DZ8O-Te4OQ/viewform?embedded=true"
-            width="640"
-            height="1084"
-            className="max-w-full"
-          >
-            Loading…
-          </iframe>
-          <Image
-            src="/alumniFundUPIid.jpg"
-            alt="An image showing Alumni Fund UPI Id and QR code"
-            width={1833}
-            height={2553}
-            className="max-w-full rounded-lg md:max-w-xl"
-          />
-        </div>
+        {/* <Helper /> */}
+        <iframe
+          src="https://docs.google.com/forms/d/e/1FAIpQLSdan_VViGmDcIufVM2EG-1E_i-Vr37jgMQMwG34DZ8O-Te4OQ/viewform?embedded=true"
+          width="640"
+          height="1084"
+          className="mx-auto max-w-full"
+        >
+          Loading…
+        </iframe>
       </section>
 
       {/* <FAQ /> */}
