93359288234ba2b1526e42ce6cf00adbb20dd909
bhendi-boi <bhendiisgreen@gmail.com>
2024-02-09 14:54:31 +0530

    Added SpeedInsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Inter, Lora } from 'next/font/google';
 import NavBar from './NavBar';
 import Footer from './Footer';
@@ -51,6 +52,7 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
         <main className="mx-auto min-h-screen max-w-7xl px-5 md:px-10">
           {children}
           <Analytics />
+          <SpeedInsights />
         </main>
         <Footer />
       </body>
